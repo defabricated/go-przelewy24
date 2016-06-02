9688d025906cf73b1cce79e954661ae3de6f4808
RadekD <radek.dejnek@gmail.com>
2016-06-02 15:49:07 +0200

    Create p24.go

---
+++ p24.go
@@ -0,0 +1,230 @@
+package przelewy24
+
+import "fmt"
+import "net/url"
+import "crypto/md5"
+import "net/http"
+import "io/ioutil"
+
+//APIVersion Current version of przelewy24 API
+const APIVersion = "3.2"
+
+var DEBUG = false
+
+type P24 struct {
+	MerchantID int
+	PosID      int
+	CRCKey     string
+
+	SessionID string
+
+	Currency string
+
+	Description string
+	Email       string
+	Country     string
+
+	ReturnURL string
+	StatusURL string
+
+	ShoppingList ShoppingList
+
+	registered bool
+	verified   bool
+}
+
+//AddProduct adds product
+func (p *P24) AddProduct(product Product) {
+	p.ShoppingList = append(p.ShoppingList, product)
+}
+
+//Amount calculates sum of products
+func (p *P24) Amount() int {
+	amount := 0
+	for _, prod := range p.ShoppingList {
+		amount = amount + prod.Price
+	}
+	return amount
+}
+
+func (p *P24) apiCall(endPoint string, values url.Values) (url.Values, error) {
+	var resp *http.Response
+	var err error
+	if DEBUG {
+		resp, err = http.PostForm("https://sandbox.przelewy24.pl/"+endPoint, values)
+	} else {
+		resp, err = http.PostForm("https://secure.przelewy24.pl/"+endPoint, values)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	m, err := url.ParseQuery(string(body))
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+//Register registers transaction
+func (p *P24) Register() (string, error) {
+	if p.registered {
+		return "", fmt.Errorf("Transaction already registered")
+	}
+	v := p.Values()
+
+	m, err := p.apiCall("trnRegister", v)
+	if err != nil {
+		return "", err
+	}
+	token := m.Get("token")
+	if token == "" {
+		errMsg := m.Get("errorMessage")
+		return "", fmt.Errorf("%s", errMsg)
+	}
+
+	p.registered = true
+
+	if DEBUG {
+		return "https://sandbox.przelewy24.pl/trnRequest/" + token, err
+	}
+	return "https://secure.przelewy24.pl/trnRequest/" + token, err
+
+}
+
+//Signature calculates digital signature for transaction
+func (p *P24) Signature() string {
+	data := fmt.Sprintf("%s|%d|%d|%s|%s", p.SessionID, p.MerchantID, p.Amount(), p.Currency, p.CRCKey)
+	return fmt.Sprintf("%x", md5.Sum([]byte(data)))
+}
+
+//Verify verifies transaction
+//map[p24_currency:[PLN] p24_method:[25] p24_sign:[a5e954561fb3fa3401c09fc39714a333] p24_session_id:[SESSIONID] p24_merchant_id:[12345] p24_pos_id:[12345] p24_statement:[p24-111-111-111] p24_amount:[123] p24_order_id:[1212345132]]
+func (p *P24) Verify(values url.Values) (bool, error) {
+	if p.verified {
+		return true, fmt.Errorf("Transaction already verified")
+	}
+
+	remoteSign := values.Get("p24_sign")
+	if remoteSign == "" {
+		return false, fmt.Errorf("Empty p24_sign")
+	}
+	orderID := values.Get("p24_order_id")
+	if orderID == "" {
+		return false, fmt.Errorf("Empty p24_order_id")
+	}
+
+	localSign := fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%s|%s|%d|%s|%s", p.SessionID, orderID, p.Amount(), p.Currency, p.CRCKey))))
+	if localSign != remoteSign {
+		return false, fmt.Errorf("Invalid p24_sign %s != %s", localSign, remoteSign)
+	}
+
+	v := url.Values{}
+	v.Add("p24_merchant_id", fmt.Sprint(p.MerchantID))
+	v.Add("p24_pos_id", fmt.Sprint(p.PosID))
+	v.Add("p24_session_id", p.SessionID)
+
+	v.Add("p24_amount", fmt.Sprint(p.Amount()))
+	v.Add("p24_currency", p.Currency)
+	v.Add("p24_order_id", values.Get("p24_order_id"))
+	v.Add("p24_sign", localSign)
+
+	m, err := p.apiCall("trnVerify", v)
+	if err != nil {
+		return false, err
+	}
+
+	token := m.Get("token")
+	if token == "" {
+		errMsg := m.Get("errorMessage")
+		return false, fmt.Errorf("%s", errMsg)
+	}
+
+	p.verified = true
+
+	return true, nil
+}
+
+//Values exports all values for requests
+func (p *P24) Values() url.Values {
+	v := url.Values{}
+
+	v.Add("p24_merchant_id", fmt.Sprint(p.MerchantID))
+	v.Add("p24_pos_id", fmt.Sprint(p.PosID))
+	v.Add("p24_session_id", p.SessionID)
+
+	v.Add("p24_amount", fmt.Sprint(p.Amount()))
+	v.Add("p24_currency", p.Currency)
+
+	v.Add("p24_description", p.Description)
+	v.Add("p24_email", p.Email)
+	v.Add("p24_country", p.Country)
+
+	v.Add("p24_url_return", p.ReturnURL)
+	if p.StatusURL != "" {
+		v.Add("p24_url_status", p.StatusURL)
+	}
+
+	v.Add("p24_api_version", APIVersion)
+	v.Add("p24_sign", p.Signature())
+
+	for key, values := range p.ShoppingList.Values() {
+		for _, value := range values {
+			v.Add(key, value)
+		}
+	}
+	return v
+}
+
+type ShoppingList []Product
+
+// Values exports all values for requests
+func (l ShoppingList) Values() url.Values {
+	v := url.Values{}
+	for i, value := range l {
+		v.Add(fmt.Sprintf("p24_name_%d", i), value.Name)
+		v.Add(fmt.Sprintf("p24_description_%d", i), value.Description)
+		v.Add(fmt.Sprintf("p24_quantity_%d", i), fmt.Sprint(value.Quantity))
+		v.Add(fmt.Sprintf("p24_price_%d", i), fmt.Sprint(value.Price))
+	}
+	return v
+}
+
+//Product represents single item
+type Product struct {
+	Name        string
+	Description string
+	Quantity    int
+	Price       int
+}
+
+func NewTransaction(MerchantID int, PosID int, CRCKey string, SessionID string, Description string, Currency string, Email string, ReturnURL string) *P24 {
+	return &P24{
+		MerchantID:  MerchantID,
+		PosID:       PosID,
+		CRCKey:      CRCKey,
+		SessionID:   SessionID,
+		Currency:    Currency,
+		Description: Description,
+
+		Email:     Email,
+		ReturnURL: ReturnURL,
+	}
+}
+
+/*
+func main() {
+	p := NewTransaction(12345, 12345, "CRCKey", "UNIQUE SESSION ID", "Descriptn", "PLN", "p24@example.com", "http://example.com/p24/success")
+	p.StatusURL = "http://example.com/api/p24_status"
+	p.AddProduct(Product{"Test product", "product description", 1, 123})
+
+	redirect_url, err := p.Register()
+	status, err := p.Verify(req.From)
+}
+*/
